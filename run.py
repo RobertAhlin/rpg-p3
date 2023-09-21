74e0738466d016b46618efcfd28b4fa4aabc6310
RobertAhlin <>
2023-09-21 09:29:02 +0000

    ask input for player's name

--- run.py
+++ run.py
@@ -12,8 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('rpg_p3')
 
-story = SHEET.worksheet('story')
+def set_player_name():
+    """
+    Ask for the players name.
+    """
+    print("Please enter your name.")
+    print("Just one name with one word.")
 
-data = story.get_all_values()
+    player_name_str = input("Enter your name: ")
+    print(f"Welcome to the game {player_name_str}.")
 
-print(data)
+set_player_name()
